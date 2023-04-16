905cbca55002f197baf2efefc842707000c41e2a
BrunoHMR <bruno_hmr@outlook.com>
2023-04-16 15:21:20 -0300

    add sweetviz report

--- bot/hotel-bot.py
+++ bot/hotel-bot.py
@@ -13,14 +13,14 @@ token = '6166531884:AAHfsnsqgkQRV4998A92K-tMnOCPYKPCQVk'
 # get updates - pega a última mensagem que eu enviei para o bot
 # https://api.telegram.org/bot6166531884:AAHfsnsqgkQRV4998A92K-tMnOCPYKPCQVk/getUpdates
 
+# send message - envia mensagem do bot para mim
+# https://api.telegram.org/bot6166531884:AAHfsnsqgkQRV4998A92K-tMnOCPYKPCQVk/sendMessage?chat_id=5507011943&text=Hi
+
 # Webhook
-# https://api.telegram.org/bot6166531884:AAHfsnsqgkQRV4998A92K-tMnOCPYKPCQVk/setWebhook?url=https://costa-del-data-api.onrender.com/costa_del_data/predict
+# https://api.telegram.org/bot6166531884:AAHfsnsqgkQRV4998A92K-tMnOCPYKPCQVk/setWebhook?url=https://costa-del-data-bot.onrender.com
 
 # Delete Webhook
 # https://api.telegram.org/bot6166531884:AAHfsnsqgkQRV4998A92K-tMnOCPYKPCQVk/deleteWebhook
-
-# send message - envia mensagem do bot para mim
-# https://api.telegram.org/bot6166531884:AAHfsnsqgkQRV4998A92K-tMnOCPYKPCQVk/sendMessage?chat_id=5507011943&text=Hi
 
 
 def send_message(chat_id, text):
